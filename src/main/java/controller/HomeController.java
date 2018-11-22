3aa8f8d679415bdfd207b3ab097b5922c7321f16
Hoang Son <sonhtd00496@fpt.edu.vn>
2018-11-22 14:46:42 +0700

    initial

---
+++ src/main/java/controller/HomeController.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/home")
+    public ModelAndView home(){
+        ModelAndView modelAndView = new ModelAndView("home");
+        modelAndView.addObject("title", "Spring MVC");
+        modelAndView.addObject("message", "Hello Spring MVC");
+        return modelAndView;
+    }
+}
